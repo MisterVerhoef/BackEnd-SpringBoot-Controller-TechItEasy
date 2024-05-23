2a19958b80516092121a0360d3c2341bb8607eb2
MisterVerhoef <frankverhoef@outlook.com>
2024-05-23 20:15:33 +0200

    basis

---
+++ src/main/java/novi/backend/backendspringbootcontrollertechiteasy/BackEndSpringBootControllerTechItEasyApplication.java
@@ -0,0 +1,13 @@
+package novi.backend.backendspringbootcontrollertechiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringBootControllerTechItEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndSpringBootControllerTechItEasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/novi/backend/backendspringbootcontrollertechiteasy/BackEndSpringBootControllerTechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package novi.backend.backendspringbootcontrollertechiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSpringBootControllerTechItEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
